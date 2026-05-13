688b5ce8436bbe0573aaf414cae5874574ceda4b
kurindinvv <kurindinvv@ERV-W0013>
2026-05-13 13:18:29 +0300

    server docker deployment for frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import type { FormEvent } from 'react'
 import './App.css'
 
 type MenuKey =
